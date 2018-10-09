e61a9d8f42b97f9817d57dfca501d8f866fed88d
vodnii <43979176+vodnii@users.noreply.github.com>
2018-10-09 09:40:39 +0300

    Update aur.sh
    добавил echo

--- aur.sh
+++ aur.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+ echo "Установка yaourt в Arch Linux."
+ echo "---@---@---@---@---@---@---@---"
+ echo "Устанавливаем дополнительные зависимости и используя репозиторий AUR скачиваем и собираем package-query, а затем yaourt."
+ echo "Поехали? (нажми пробел)"
+ read item
+ case "$item" in
+       space) echo "Есть!"
+        ;;
+ esac
 sudo pacman -S --needed base-devel git wget yajl
 cd /tmp
 git clone https://aur.archlinux.org/package-query.git
@@ -10,3 +19,11 @@ cd yaourt/
 makepkg -si
 cd ..
 sudo rm -dR yaourt/ package-query/
+ echo "screenfetch (нажми пробел)"
+ read item
+ case "$item" in
+       *) echo "Нет"
+        exit 0
+       space) echo "Есть!"
+        ;;
+ esac
